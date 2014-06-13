fcfb81643e902fc79538404781c062a57952ecc9
Barbora Duskov <duskovab@gmail.com>
2014-06-13 18:45:23 +0200

    Změna menu

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def index 
+  end
+
+  def me 
+  end
+  
+  def kontakt 
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/me'
+
+  get 'pages/kontakt'
+
+  root to: 'pages#index'
+
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
